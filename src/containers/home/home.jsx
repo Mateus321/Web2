93f13baa3f35fabb10592eea42e4fc687f13ebcd
Mateus Pereira Fernandes <mateuspereirafernandes@gmail.com>
2024-03-31 17:43:05 -0300

    Primeiro commit

---
+++ src/containers/home/home.jsx
@@ -0,0 +1,33 @@
+import SarNet from '../../assets/SarNet.svg'; 
+import './home.css';
+
+export function Home() {
+  return (
+    <div className="container">
+        <header className="header">
+            <img src={ SarNet} alt="sarnet" />
+            <span>Sistema de agendamento de reuniões</span>
+        </header>
+
+        <form>
+            <div className="inputContainer">
+                <label htmlFor="name">Nome</label>
+                <input type="text" id="name" name="name" placeholder="Digite seu nome"/>
+            </div>
+
+            <div className="inputContainer">
+                <label htmlFor="codAcess">Codigo de acesso</label>
+                <input type="text" id="codAcess" name="codAcess" placeholder="Digite o codigo de acesso"/>
+            </div>
+
+            <a href="">Criar sua planilha de reuniões</a>
+
+            <button className="button">
+                Acessar 
+            </button>
+        </form>  
+
+    </div>
+  );
+}
+export default Home;
